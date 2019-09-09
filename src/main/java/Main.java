17f8a8a702f5c317de3cc50872db52d537819b4a
Milan <milanlopatin@gmail.com>
2019-09-09 18:13:01 +0400

    no message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws SQLException, IOException {
         //DatabaseOperations.createStudentsTable();
-        DatabaseOperations.fillStudentsTable();
+        //DatabaseOperations.fillStudentsTable();
         DatabaseOperations.showStudentsTable();
     }
 }
